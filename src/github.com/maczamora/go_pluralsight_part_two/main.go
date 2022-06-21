9ad44adf6ac186d2e987a36478d8566998cd8dda
Mario Zamora <mario.a.zamora@outlook.com>
2022-06-21 17:09:48 -0600

    Saving part two content

--- src/github.com/maczamora/go_pluralsight_part_two/main.go
+++ src/github.com/maczamora/go_pluralsight_part_two/main.go
@@ -12,4 +12,14 @@ func main() {
 
 	firstName := "Mario" // implicit inticialization and will  mostly see this in Go
 	fmt.Println(firstName)
+
+	boolTest := true
+	fmt.Println(boolTest)
+
+	c := complex(3, 4)
+	fmt.Println(c)
+
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
+
 }
